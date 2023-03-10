84151792ad46414d9ca98e939f34cccacd374528
root <root@DESKTOP-5H6GJ96>
2023-03-10 17:53:44 -0600

    still some bugs on database

--- CS50FINALE/STORE50/app.py
+++ CS50FINALE/STORE50/app.py
@@ -1,29 +1,42 @@
+import os
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from cs50 import SQL
-from flask_session import Session
-
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"]) 
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///store.db")
+
+
+@app.route("/", methods=["GET"])
 def home():
-    return render_template("layout.html")
-    
-@app.route("/newReleases", methods=["GET"]) 
+    return render_template("home.html")
+
+
+@app.route("/newReleases", methods=["GET"])
 def newReleases():
     return render_template("newReleases.html")
 
-@app.route("/shopping", methods=["GET"]) 
+
+@app.route("/shopping", methods=["GET"])
 def shopping():
     return render_template("shopping.html")
 
-@app.route("/aboutUs", methods=["GET"]) 
+
+@app.route("/aboutUs", methods=["GET"])
 def about():
     return render_template("aboutUs.html")
 
-@app.route("/contact", methods=["GET"]) 
+
+@app.route("/contact", methods=["GET"])
 def contact():
     return render_template("contact.html")
 
 
+@app.route("/devMode", methods=["GET"])
+def dev():
+
+    return render_template("devMode.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
+
